44991c74f559baaaff0e31ef66dd7a11d2e802c2
LAPTOP-MKKARI3G\bigllxx <bigllxx@163.com>
2022-05-22 05:17:10 +0800

    。

--- 3、串/2、模式匹配和KMP算法.cpp
+++ 3、串/2、模式匹配和KMP算法.cpp
@@ -4,12 +4,12 @@
 
 int Index(SString S, SString T){  //朴素模式匹配
   int i=1,j=1;
-  while(i<S.length && j<T.length){
+  while(i<=S.length && j<=T.length){
     if(S.ch[i]==T.ch[j]){
       ++i;
       ++j;
     }else{
-      i=i-j+2;
+      i=i-j+2;  // 让i指向S数组的下一个位置，这里不理解的话，可以自动定义一个新的变量t，指向S的下一个位置
       j=1;
     }
   }
@@ -22,9 +22,9 @@ int Index(SString S, SString T){  //朴素模式匹配
 
 void getNext(SString T, int next[]){  // 求next数组
   int i=1,j=0;
-  next[1]=0;
+  next[1]=0;  // 这里的next数组从1开始，0不存数据
   while(i<T.length){
-    if(j==0 || T.ch[i]==T.ch[j]){
+    if(j==0 || T.ch[i]==T.ch[j]){  // 0 ，1 ， 
       ++i;++j;
       next[i]=j;
     }else{
@@ -34,11 +34,11 @@ void getNext(SString T, int next[]){  // 求next数组
 }
 
 int IndexKMP(SString S, SString T){  // KMP算法
-  int i=1,j=1;
-  int next[T.length+1];
+  int i=1,j=1,n=T.length+1;
+  int next[n];
   getNext(T, next);
   while(i<=S.length && j<=T.length){
-    if(j==0 || s.ch[i]==T.ch[i]){
+    if(j==0 || S.ch[i]==T.ch[i]){
       ++i;++j;
     }else{
       j=next[j];
